f4c3f0078fdc0e3cd5462d5a7415ce31ff45a791
Steve Ngo <stevehngo@hotmail.com>
2022-01-14 03:46:29 -0500

    tests

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,16 +1,33 @@
 'use strict';
 
-
-const server = require('../app.js');
+const server = require('../src/server.js');
 const supertest = require('supertest');
 const request = supertest(server.app);
 
 describe('Testing my HTTP server', () => {
 
-  it('Should be able to response to a POST to /message', async () => {
-    let response = await request.post('/message?text=test&author=test');
+  it('Should be able to respond to a GET to /person', async () => {
+
+    let response = await request.get('/person?name=test');
 
     expect(response.status).toEqual(200);
-    expect(response.body[0].text).toEqual('test');
+    expect(response.body.name).toEqual('test');
+  });
+
+  it('Should be able to 500 if no name in the query string.', async () => {
+    let response = await request.get('/person?name=');
+
+    expect(response.status).toEqual(500);    
+  });
+  
+  it('Should be able to 404 on a bad route.', async () => {
+    let response = await request.post('/person');
+
+    expect(response.status).toEqual(404);
+  });
+  it('Should be able to 404 on a bad method.', async () => {
+    let response = await request.get('/badroute');
+
+    expect(response.status).toEqual(404);
   });
 });
---
+++ __test__/validator.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const server = require('../src/server.js');
+const supertest = require('supertest');
+const request = supertest(server.app);
+
+describe('Testing my validator', () => {
+  
+});
